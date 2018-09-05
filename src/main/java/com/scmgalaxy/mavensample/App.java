f0eec7e63edcd5fda25677bbffd3e42919a08563
Balaji1218 <37077189+Balaji1218@users.noreply.github.com>
2018-09-05 14:23:09 +0530

    fix for TEM-2
    fix for TEM-2 fix for TEM-2

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! TEM-2 modified" );
+        System.out.println( "Hello World RefX! TEM-2 modifiedd" );
     }
 }
